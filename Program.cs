46e32134bf23ede1ca3b9fed7f5f7d78ee35c030
Popova Viktoria <popova_ak24@nuwm.edu.ua>
2025-10-14 16:43:18 +0300

    Refactor Vector4D and Matrix classes to implement IMathObject

--- Program.cs
+++ Program.cs
@@ -1,23 +1,36 @@
 using System;
 
+    public interface IMathObject
+    {
+        void SetElements();
+        void Display();
+        double FindMax();
+    }
+
+
 namespace Lab4
 {
-    // Базовий клас одновимірного вектора розмірності 4
-    public class Vector4D
+    public static class MathConstants
+    {
+        public const int VECTOR_SIZE = 4;
+        public const int MATRIX_SIZE = 4;
+    }
+    // Базовий клас одновимірного вектора
+    public class Vector4D : IMathObject
     {
         protected double[] elements; // Масив елементів вектора
 
         // Конструктор
         public Vector4D()
         {
-            elements = new double[4];
+            elements = new double[MathConstants.VECTOR_SIZE];
         }
 
-        // Віртуальний метод для задання елементів вектора
-        public virtual void SetElements()
+        // Встановлення елементів з консолі
+        public void SetElements()
         {
-            Console.WriteLine("Введіть 4 елементи вектора:");
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine($"Введіть {MathConstants.VECTOR_SIZE} елементи вектора:");
+            for (int i = 0; i < MathConstants.VECTOR_SIZE; i++)
             {
                 Console.Write($"Елемент [{i}]: ");
                 while (!double.TryParse(Console.ReadLine(), out elements[i]))
@@ -27,61 +40,66 @@ namespace Lab4
             }
         }
 
-        // Метод для задання елементів з масиву (для тестування)
-        public virtual void SetElements(double[] values)
+        // Встановлення елементів з масиву
+        public void SetElements(double[] values)
         {
-            if (values.Length == 4)
+            if (values.Length == MathConstants.VECTOR_SIZE)
             {
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < MathConstants.VECTOR_SIZE; i++)
                 {
                     elements[i] = values[i];
                 }
             }
         }
 
-        // Віртуальний метод для виведення вектора на екран
-        public virtual void Display()
+        // Виведення вектора
+        public void Display()
         {
             Console.Write("Вектор: [");
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < MathConstants.VECTOR_SIZE; i++)
             {
                 Console.Write(elements[i]);
-                if (i < 3) Console.Write(", ");
+                if (i < MathConstants.VECTOR_SIZE - 1)
+                {
+                    Console.Write(", ");
+                }
             }
             Console.WriteLine("]");
         }
 
-        // Віртуальний метод для знаходження максимального елемента
-        public virtual double FindMax()
+        // Пошук максимального елемента
+        public double FindMax()
         {
             double max = elements[0];
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < MathConstants.VECTOR_SIZE; i++)
             {
                 if (elements[i] > max)
+                {
                     max = elements[i];
+                }
             }
             return max;
         }
     }
 
     // Похідний клас матриці 4x4
-    public class Matrix : Vector4D
+        public class Matrix : IMathObject
     {
         private double[,] matrix; // Двовимірний масив для матриці
 
         // Конструктор
         public Matrix()
         {
-            matrix = new double[4, 4];
+            matrix = new double[MathConstants.MATRIX_SIZE, MathConstants.MATRIX_SIZE];
         }
 
         // Перевантажений метод для задання елементів матриці
-        public override void SetElements()
+            public void SetElements()
         {
-            Console.WriteLine("Введіть елементи матриці 4x4:");
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine($"Введіть елементи матриці {MathConstants.MATRIX_SIZE}x{MathConstants.MATRIX_SIZE}:");
+            for (int i = 0; i < MathConstants.MATRIX_SIZE; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < MathConstants.MATRIX_SIZE; j++)
                 {
                     Console.Write($"Елемент [{i},{j}]: ");
                     while (!double.TryParse(Console.ReadLine(), out matrix[i, j]))
@@ -95,11 +113,11 @@ namespace Lab4
         // Метод для задання елементів з двовимірного масиву (для тестування)
         public void SetElements(double[,] values)
         {
-            if (values.GetLength(0) == 4 && values.GetLength(1) == 4)
+            if (values.GetLength(0) == MathConstants.MATRIX_SIZE && values.GetLength(1) == MathConstants.MATRIX_SIZE)
             {
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < MathConstants.MATRIX_SIZE; i++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    for (int j = 0; j < MathConstants.MATRIX_SIZE; j++)
                     {
                         matrix[i, j] = values[i, j];
                     }
@@ -108,13 +126,13 @@ namespace Lab4
         }
 
         // Перевантажений метод для виведення матриці на екран
-        public override void Display()
+            public void Display()
         {
-            Console.WriteLine("Матриця 4x4:");
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine($"Матриця {MathConstants.MATRIX_SIZE}x{MathConstants.MATRIX_SIZE}:");
+            for (int i = 0; i < MathConstants.MATRIX_SIZE; i++)
             {
                 Console.Write("| ");
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < MathConstants.MATRIX_SIZE; j++)
                 {
                     Console.Write($"{matrix[i, j]:F2} ");
                 }
@@ -123,15 +141,17 @@ namespace Lab4
         }
 
         // Перевантажений метод для знаходження максимального елемента матриці
-        public override double FindMax()
+            public double FindMax()
         {
             double max = matrix[0, 0];
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < MathConstants.MATRIX_SIZE; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < MathConstants.MATRIX_SIZE; j++)
                 {
                     if (matrix[i, j] > max)
+                    {
                         max = matrix[i, j];
+                    }
                 }
             }
             return max;
@@ -149,26 +169,13 @@ namespace Lab4
             // Вибір режиму роботи
             Console.WriteLine("Виберіть режим роботи:");
             Console.WriteLine("1 - Ввід з клавіатури");
-            Console.WriteLine("2 - Демонстрація з тестовими даними");
-            Console.Write("Ваш вибір (1 або 2): ");
-            
+            Console.Write("Ваш вибір (1): ");
+
             string choice = Console.ReadLine();
-            
+
             try
             {
-                if (choice == "1")
-                {
-                    RunInteractiveMode();
-                }
-                else if (choice == "2")
-                {
-                    RunTestMode();
-                }
-                else
-                {
-                    Console.WriteLine("Некоректний вибір. Запускаю режим демонстрації.");
-                    RunTestMode();
-                }
+                RunInteractiveMode();
             }
             catch (Exception ex)
             {
@@ -192,7 +199,7 @@ namespace Lab4
             Console.WriteLine("1. Робота з одновимірним вектором розмірності 4:");
             Console.WriteLine(new string('-', 50));
             
-            Vector4D vector = new Vector4D();
+                IMathObject vector = new Vector4D();
             vector.SetElements();
             
             Console.WriteLine("\nРезультат:");
@@ -206,7 +213,7 @@ namespace Lab4
             Console.WriteLine("2. Робота з матрицею 4x4:");
             Console.WriteLine(new string('-', 50));
             
-            Matrix matrix = new Matrix();
+                IMathObject matrix = new Matrix();
             matrix.SetElements();
             
             Console.WriteLine("\nРезультат:");
@@ -225,9 +232,8 @@ namespace Lab4
             Console.WriteLine("1. Робота з одновимірним вектором розмірності 4:");
             Console.WriteLine(new string('-', 50));
             
-            Vector4D vector = new Vector4D();
+                IMathObject vector = new Vector4D();
             double[] testVector = { 1.5, 8.3, 3.7, 5.2 };
-            vector.SetElements(testVector);
             
             Console.WriteLine("Тестові дані вектора: [1.5, 8.3, 3.7, 5.2]");
             Console.WriteLine("\nРезультат:");
@@ -241,14 +247,13 @@ namespace Lab4
             Console.WriteLine("2. Робота з матрицею 4x4:");
             Console.WriteLine(new string('-', 50));
             
-            Matrix matrix = new Matrix();
+                IMathObject matrix = new Matrix();
             double[,] testMatrix = {
                 { 2.1, 4.5, 1.8, 3.3 },
                 { 7.2, 9.6, 2.4, 5.7 },
                 { 1.1, 3.8, 12.5, 4.2 },
                 { 6.3, 2.9, 8.1, 1.7 }
             };
-            matrix.SetElements(testMatrix);
             
             Console.WriteLine("Тестові дані матриці:");
             Console.WriteLine("2.1  4.5  1.8  3.3");
@@ -262,6 +267,7 @@ namespace Lab4
             Console.WriteLine($"Максимальний елемент матриці: {matrixMax}");
             
             ShowSummary(vectorMax, matrixMax);
+                // Тестовий режим видалено. Програма працює лише в інтерактивному режимі.
         }
 
         static void ShowSummary(double vectorMax, double matrixMax)
@@ -272,11 +278,17 @@ namespace Lab4
             Console.WriteLine($"Максимальний елемент матриці: {matrixMax}");
             
             if (vectorMax > matrixMax)
+            {
                 Console.WriteLine("Максимальний елемент знаходиться у векторі.");
+            }
             else if (matrixMax > vectorMax)
+            {
                 Console.WriteLine("Максимальний елемент знаходиться у матриці.");
+            }
             else
+            {
                 Console.WriteLine("Максимальні елементи вектора та матриці рівні.");
+            }
         }
     }
 }
